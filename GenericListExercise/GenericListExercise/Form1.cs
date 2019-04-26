1de05d4f233522f5fe75267392e014fbe11edf3a
Jeremy <Jeremy.stevnes@gmail.com>
2019-04-26 12:21:26 -0300

    Struct and formload
    Added Grades struct and loaded the allGrades ArrayList  with grades in the formload

--- GenericListExercise/GenericListExercise/Form1.cs
+++ GenericListExercise/GenericListExercise/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,57 @@ using System.Windows.Forms;
 
 namespace GenericListExercise
 {
+    public struct Grades
+    {
+        public string GradeLetter;
+        public int MaxGrade;
+    }
+
     public partial class Form1 : Form
     {
+        private Grades grade;
+        ArrayList AllGrades = new ArrayList();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                grade.MaxGrade = 299;
+                grade.GradeLetter = "F";
+
+                AllGrades.Add(grade);
+
+                grade.MaxGrade = 349;
+                grade.GradeLetter = "D";
+
+                AllGrades.Add(grade);
+
+                grade.MaxGrade = 399;
+                grade.GradeLetter = "C";
+
+                AllGrades.Add(grade);
+
+                grade.MaxGrade = 449;
+                grade.GradeLetter = "B";
+
+                AllGrades.Add(grade);
+
+                grade.MaxGrade = 500;
+                grade.GradeLetter = "A";
+
+                AllGrades.Add(grade);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
     }
+    
+
 }
